e5caa3bff2cc0724b4a762a994edfceadfdebbb4
henquesz <henquesvinicius@gmail.com>
2022-11-23 10:07:53 -0300

    refaturação das rotas res de cors // 2

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,6 @@
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }, req, res) {
-  res.setHeader('Cache-Control', 's-maxage-10, stale-while-revalidate');
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
