b1735795ef16822564745d5b7c8b0e6507427781
henquesz <henquesvinicius@gmail.com>
2022-11-23 10:13:03 -0300

    teste de cors com nextcors lib

--- pages/api/poke.js
+++ pages/api/poke.js
@@ -1,12 +1,17 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import NextCors from 'nextjs-cors';
 
 export default async function handler(req, res) {
   // res.status(200).json({ name: 'John Doe' })
   const pokeApi = await fetch("https://pokeapi.co/api/v2/pokemon?limit=18");
   const pokeResponseJson = await pokeApi.json();
     res.setHeader('Cache-Control', 's-maxage-10, stale-while-revalidate');
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    await NextCors(req, res, {
+      // Options
+      methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+      origin: '*',
+      optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+   });
       res.json({
           pokemon: pokeResponseJson
       })
