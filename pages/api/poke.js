00c5542a37cd7051ecc931f2bc157803f4b3d51f
henquesz <henquesvinicius@gmail.com>
2022-11-23 11:23:30 -0300

    refaturando

--- pages/api/poke.js
+++ pages/api/poke.js
@@ -2,10 +2,22 @@
 
 export default async function handler(req, res) {
   // res.status(200).json({ name: 'John Doe' })
+
+  res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, OPTIONS, DELETE');
+
   const pokeApi = await fetch("https://pokeapi.co/api/v2/pokemon?limit=18");
   const pokeResponseJson = await pokeApi.json();
       res.json({
           pokemon: pokeResponseJson
       })
+
+      if(req.method === 'OPTIONS') {
+        return res.status(200).json(({
+          pokemon: pokeResponseJson
+        }))
+    }
   console.log(pokeResponseJson);
 }
