1635bf2696064af1b32545cf5a66e0db624ad326
henquesz <henquesvinicius@gmail.com>
2022-11-23 09:48:10 -0300

    access control allow headers inclusion

--- pages/api/poke.js
+++ pages/api/poke.js
@@ -4,9 +4,11 @@ export default async function handler(req, res) {
   // res.status(200).json({ name: 'John Doe' })
   const pokeApi = await fetch("https://pokeapi.co/api/v2/pokemon?limit=18");
   const pokeResponseJson = await pokeApi.json();
-  res.setHeader('Cache-Control', 's-maxage-10, stale-while-revalidate');
-    res.json({
-        pokemon: pokeResponseJson
-    })
-    console.log(pokeResponseJson);
+    res.setHeader('Cache-Control', 's-maxage-10, stale-while-revalidate');
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      res.json({
+          pokemon: pokeResponseJson
+      })
+  console.log(pokeResponseJson);
 }
