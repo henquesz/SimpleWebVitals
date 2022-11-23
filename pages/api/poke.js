2f0d6d696b0a7ee489f88d3ca4904d6070381566
henquesz <henquesvinicius@gmail.com>
2022-11-23 10:55:25 -0300

    modificações na lógica de cors // next.config.js

--- pages/api/poke.js
+++ pages/api/poke.js
@@ -4,8 +4,6 @@ export default async function handler(req, res) {
   // res.status(200).json({ name: 'John Doe' })
   const pokeApi = await fetch("https://pokeapi.co/api/v2/pokemon?limit=18");
   const pokeResponseJson = await pokeApi.json();
-
-    res.setHeader('Cache-Control', 's-maxage-10, stale-while-revalidate', 'Access-Control-Allow-Origin', '*', 'Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
       res.json({
           pokemon: pokeResponseJson
       })
