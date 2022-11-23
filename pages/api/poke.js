827b72648f1dc0cb84cf4cf261dbfc2ce61ff9a3
henquesz <henquesvinicius@gmail.com>
2022-11-23 10:28:14 -0300

    refaturação para padrão

--- pages/api/poke.js
+++ pages/api/poke.js
@@ -1,17 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import NextCors from 'nextjs-cors';
 
 export default async function handler(req, res) {
   // res.status(200).json({ name: 'John Doe' })
   const pokeApi = await fetch("https://pokeapi.co/api/v2/pokemon?limit=18");
   const pokeResponseJson = await pokeApi.json();
-    res.setHeader('Cache-Control', 's-maxage-10, stale-while-revalidate');
-    await NextCors(req, res, {
-      // Options
-      methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-      origin: '*',
-      optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-   });
+
+    res.setHeader('Cache-Control', 's-maxage-10, stale-while-revalidate', 'Access-Control-Allow-Origin', '*', 'Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
       res.json({
           pokemon: pokeResponseJson
       })
